c03906da6c36d4deffde377d2b43fc73bd01b4ec
dleeftink <dennisleeftink@gmail.com>
2023-08-24 18:54:16 +1000

    Extra spacing

--- src/rank/minsum.js
+++ src/rank/minsum.js
@@ -3,52 +3,53 @@ export default function minsum (uint, val = 1) {
   let invalid = this.invalid;
 
   // range guard
-  if( uint < this.clip || uint > this.span || invalid(uint) || invalid(val)) return;
+  if ( uint < this.clip || uint > this.span || invalid(uint) || invalid(val)) return
 
   var old = this.bits[uint];
       val = old + val;
 
   // count guard
-  if (val > this.high) {
-    return;
-  }
+  if ( val > this.high ) return
 
   this.bits[uint] = val;
   let  rank = this.rank,
        stat = this.stat;
 
-  if (val > this.tmin && val > this.freq) {
+  if ( val > this.tmin && val > this.freq ) {
 
     var slot = this.slot;
     var last = this.last;
 
     //let idx = rank.indexOf(uint); // -> slower
-    for (var idx = 0; idx < slot; ++idx) {
-      if (rank[idx] == uint) {
+    for ( var idx = 0; idx < slot; ++idx ) {
+      if ( rank[idx] == uint ) {
         break;
-      } else if (idx == last) {
+      } else if ( idx == last ) {
         idx = -1;
         break;
       }
     }
 
-    if (idx >= 0) {
+    if ( idx >= 0 ) {
       stat[idx] = val;
     } else {
+
       var low = this.tmin;
-      for (var ins = 0; ins < slot; ++ins) {
-        if (stat[ins] <= low) {
-          break;
-        }
+      for ( var ins = 0; ins < slot; ++ins ) {
+       if ( stat[ins] <= low ) break
       }
 
       rank[ins] = uint;
       stat[ins] = val;
 
+      // TO DO: autotuning in case freq settings == 0
       //if((this.tmin = lowest(stat)) < this.freq ) { this.tmin = this.freq } // => Math.max(lowest(stat),this.freq)
-      this.tmin = this.lowest(stat); // TO DO: autotuning in case freq settings == 0
+      this.tmin = this.lowest(stat); 
+      
     }
+
     if (val > this.tmax) this.tmax = val;
   }
+
   // return this
 };
--- src/rank/winsum.js
+++ src/rank/winsum.js
@@ -3,16 +3,18 @@ export default function winsum(uint,val = 1) {
   let invalid = this.invalid;
 
   // range guard  
-  if ( uint < this.clip || uint > this.span || invalid(uint) || invalid(val)) return;
+  if ( uint < this.clip || uint > this.span || invalid(uint) || invalid(val)) return
 
   var old = this.bits[uint];
       val = old + val; 
   
   // count guard
-  if( val > this.high ) { return } 
+  if ( val > this.high ) return
+
   this.bits[uint] = val;
-  
-  let rank = this.rank , stat = this.stat ;
+  let  rank = this.rank, 
+       stat = this.stat;
+
   if ( val > this.tmin && val > this.freq ) {
     
     var slot = this.slot;
@@ -26,7 +28,7 @@ export default function winsum(uint,val = 1) {
 
    }
 
-   if(ins == undefined) return
+   if ( ins == undefined ) return
      
    if ( idx >= 0 ) {
     if ( ins > idx ) return // TO DO: overwrite buffer when restarting count to prevent having to .fill(0)
@@ -45,8 +47,9 @@ export default function winsum(uint,val = 1) {
       rank[ins] = uint;
       stat[ins] = val;
 
+      // TO DO: autotuning in case freq settings == 0
       this.tmin = stat[this.last] ?? 0  // min always last // Math.max(this.freq,stat[this.last])
-       // TO DO: autotuning in case freq settings == 0
+      
        
     }
 
