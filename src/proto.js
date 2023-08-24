f1add8040a1ef645597020028f34f9857b5d79d7
dleeftink <dennisleeftink@gmail.com>
2023-08-25 02:39:58 +1000

    Update proto.js

--- src/proto.js
+++ src/proto.js
@@ -1,18 +1,14 @@
 import batch from './core/batch.js';
 import clear from './core/clear.js';
-
 import minsum from './rank/minsum.js';
 import winsum from './rank/winsum.js';
 import derank from './rank/derank.js';
 import unique from './core/unique.js';
 import lowest from './util/lowest.js';
-
 import expects from './util/expects.js';
 import invalid from './util/invalid.js';
-
 import { get, has } from './core/getters.js';
 import { add, del, put } from './core/setters.js';
-
 import { keys, values, entries } from './core/sorters.js';
 
 const prototype = {
