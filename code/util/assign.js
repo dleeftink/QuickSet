d84a651c5b0a35e164e41bd48955d6cad348e58d
dleeftink <dennisleeftink@gmail.com>
2023-09-12 03:50:32 +1000

    Update assign.js

--- code/util/assign.js
+++ code/util/assign.js
@@ -3,7 +3,7 @@ function escapeRegExp(string) {
 }
 
 // only inlines Class methods
-// no nested functions inside methods
+// not nested functions inside methods
 
 export default (Class, methods, params, raw = false) => {
   let source = Class.toString();
