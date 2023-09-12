fa112768bb4ce73bf63a5b86472fe03fd062cdbc
dleeftink <dennisleeftink@gmail.com>
2023-09-13 00:53:41 +1000

    Update invalid.js

--- code/util/invalid.js
+++ code/util/invalid.js
@@ -1,6 +1,6 @@
 // why is this faster on stackblitz?
 export default function invalid(input) {
-  // rightshift max 32-bit input
+  // rightshift 32-bit input
   input >>>= 1;
   return (input ^ 0) !== input;
 }
